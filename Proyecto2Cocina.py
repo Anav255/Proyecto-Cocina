1d161bedab6a16e8a346382da827bd757f986741
Ana Araya <victoriaanaaraya@gmail.com>
2026-05-22 13:49:37 -0600

    Primer commit de prueba

---
+++ Proyecto2Cocina.py
@@ -0,0 +1,12 @@
+### Juego cocina ###
+import random
+import time
+import os
+import sys
+import pygame
+from pygame.locals import *
+pygame.init()
+# Configuración de la pantalla
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Juego de Cocina")   
