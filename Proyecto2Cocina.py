11265bffdadbe96fa7f1fc794134cd8fd3d4b9a2
Ana Araya <victoriaanaaraya@gmail.com>
2026-05-26 13:18:56 -0600

    Imagenes de los ingredientes

--- Proyecto2Cocina.py
+++ Proyecto2Cocina.py
@@ -1,12 +1,27 @@
 ### Juego cocina ###
-import random
-import time
-import os
-import sys
 import pygame
 from pygame.locals import *
+import time
+import random
 pygame.init()
-# Configuración de la pantalla
+# Pantalla juego
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Juego de Cocina")   
+
+FPS = 60
+running = True
+
+def main(window):
+    clck = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+        window.fill((255, 255, 255))
+        clck.tick(FPS)
+
+
+        pygame.display.flip()
+pygame.quit()
