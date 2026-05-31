95c0b9c132bcb717e54a992f511f38d47d82d960
Anav255 <victoriaanaaraya@gmail.com>
2026-05-30 21:12:30 -0600

    nuevas clases

--- Proyecto2Cocina.py
+++ Proyecto2Cocina.py
@@ -13,7 +13,6 @@ FPS = 60
 running = True
 
 def main(window):
-    clck = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -21,6 +20,34 @@ def main(window):
                 return
         window.fill((255, 255, 255))
         clck.tick(FPS)
+
+
+class ingredientes:
+    def __init__(self, Lechuga, Tomate, Cebolla, Pan,):
+        pass 
+class Ensalada:
+    #El nombre de las ensaladas lo estoy definiendo según los ingredientes siendo L = Lechuga T= Tomate C= Cebolla, por cualquier cosa de que los vaya a tener que usar  
+     
+    def __init__(self, EnsaladaLT, EnsaladaLC, EnsaladaLTC):
+        self.EnsaladaLT = [self.Lechuga, self.Tomate,]
+        self.EnsaladaLT = [self.Lechuga, self.Cebolla,]
+        self.EnsaladaLT = [self.Lechuga, self.Tomate, self.Cebolla]
+        
+        pass
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         pygame.display.flip()
